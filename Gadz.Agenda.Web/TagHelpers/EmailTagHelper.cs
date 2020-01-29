9f67cf3ff30de42576b75efd3cea0f5912d0bdae
Paulo Duarte <phduarte87@outlook.com>
2020-01-29 14:28:58 -0300

    Adicionado EmailTagHelper

---
+++ Gadz.Agenda.Web/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace Gadz.Agenda.Web.TagHelpers
+{
+    [HtmlTargetElement("email")]
+    public class EmailTagHelper : TagHelper
+    {
+        public string To { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";                                 // Replaces <email> with <a> tag
+            var content = await output.GetChildContentAsync();
+            var name = content.GetContent();
+            output.Attributes.SetAttribute("href", $"mailto:{To}@gadesi.com.br");
+            output.Content.SetContent(name);
+        }
+    }
+}
