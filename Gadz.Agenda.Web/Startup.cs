a000ef00d1188a93731747d2241ad52422ea3cd6
Paulo Duarte <phduarte87@outlook.com>
2020-01-28 21:19:12 -0300

    Forçando o roteador gerar apenas URL minúsculas

--- Gadz.Agenda.Web/Startup.cs
+++ Gadz.Agenda.Web/Startup.cs
@@ -14,6 +14,7 @@ namespace Gadz.Agenda.Web
         {
             services.AddMvc();
             services.AddScoped<IUsuarioServices, AccessServices>();
+            services.AddRouting(x => x.LowercaseUrls = true);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
