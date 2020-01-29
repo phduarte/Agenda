6b68f7f853f5229a94a2c524e94ccfe70a56640c
Paulo Duarte <phduarte87@outlook.com>
2020-01-28 21:15:29 -0300

    Adicionado a controller de usuários

---
+++ Gadz.Agenda.Web/Controllers/UsuariosController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gadz.Agenda.Web.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Pesquisar(string criterio)
+        {
+            return View();
+        }
+    }
+}
