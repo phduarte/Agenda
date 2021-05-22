1cb4ed2494cb85eeabb5b4859435e1aa1774baff
Paulo Duarte <phduarte87@outlook.com>
2021-05-22 00:22:34 -0300

    Inclusão de campos

--- Gadz.Agenda.Web/Models/AgendamentoViewModel.cs
+++ Gadz.Agenda.Web/Models/AgendamentoViewModel.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Gadz.Agenda.Web.Models
+﻿namespace Gadz.Agenda.Web.Models
 {
-    public class AgendamentoViewModel : Agendamento
+    public class AgendamentoViewModel
     {
+        public string TarefaId { get; set; }
+        public string Tarefa { get; set; }
+        public string Cliente { get; set; }
+        public string Responsavel { get; set; }
         public string Categoria { get; set; }
+        public string Duracao { get; set; }
+        public string DataSolicitacao { get; set; }
+        public string MotivoTarefa { get; set; }
+        public string MotivoAgendamento { get; set; }
     }
 }
